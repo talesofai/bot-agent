16f590191ee49aac73b884810ba46a67fbea7bbf
nieta-zjj <zjj@nieta.art>
2026-01-12 12:40:12 +0800

    chore(lint): ignore data and commitlint config
    exclude data artifacts from eslint scanning
    skip commitlint config file in linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ["data/**", "dist/**", "node_modules/**", "commitlint.config.js"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
